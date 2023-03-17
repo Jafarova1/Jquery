c288e0a05dcbfd4102cb3e84c33439bc60eb7c3b
jafarova1 <gultajvj@code.edu.az>
2023-03-17 13:08:03 -0700

    added file

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
         $(".open").click(function(){
                 $(".sidebar").removeClass("hide")
                 $(this).addClass("d-none")
-                $(".close").removeClass6("d-none")
+                $(".close").removeClass("d-none")
         })
         $(".close").click(function(){
                 $(".sidebar").addClass("hide")
